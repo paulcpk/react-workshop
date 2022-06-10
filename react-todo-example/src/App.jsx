05cd074c77ef23dbd2a65fe54b7ebb4c46d167ce
Paul Csapak <paul@base34.com>
2022-06-10 12:55:10 +0200

    react-todo-example: finalize component setup and styles

--- react-todo-example/src/App.jsx
+++ react-todo-example/src/App.jsx
@@ -1,106 +1,119 @@
-import "./App.css";
-import { useState, useRef } from "react";
-import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
-import { Container } from "@mui/material";
-import TodoList from "./components/TodoList";
-import TodoForm from "./components/TodoForm";
-import { lightGreen } from "@mui/material/colors";
+import './App.css'
+import { useState, useRef } from 'react'
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
+import { Container, Paper, Stack } from '@mui/material'
 
-const darkTheme = createTheme({ palette: { mode: "dark" } });
-const lightTheme = createTheme({ palette: { mode: "light" } });
+const darkTheme = createTheme({ palette: { mode: 'dark' } })
+const lightTheme = createTheme({ palette: { mode: 'light' } })
 
 const DEFAULT_TASKS = [
   {
     id: 1,
-    task: "Finish homework",
+    task: 'Finish homework',
     completed: false,
   },
   {
     id: 1,
-    task: "Wash dishes",
+    task: 'Wash dishes',
     completed: false,
   },
   {
     id: 1,
-    task: "Clean room",
+    task: 'Clean room',
     completed: false,
   },
   {
     id: 1,
-    task: "Make waffles",
+    task: 'Make waffles',
     completed: false,
   },
-];
+]
 
-const DEFAULT_ID = 42;
+const DEFAULT_ID = 42
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  color: theme.palette.text.secondary,
+}))
 
 const App = () => {
-  const [tasks, setTasks] = useState(DEFAULT_TASKS);
-  const [formInput, setFormInput] = useState("");
-  const taskId = useRef(DEFAULT_ID);
+  const [tasks, setTasks] = useState(DEFAULT_TASKS)
+  const [formInput, setFormInput] = useState('')
+  const taskId = useRef(DEFAULT_ID)
 
   const handleChange = (e) => {
-    setFormInput(e.target.value);
-  };
+    setFormInput(e.target.value)
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    if (formInput !== "") {
+    e.preventDefault()
+    if (formInput !== '') {
       const newTask = {
         id: taskId,
         task: formInput,
         completed: false,
-      };
-      console.log(newTask);
+      }
+      console.log(newTask)
       // increment taskId
-      taskId.current++;
-      setTasks([...tasks, newTask]);
-      setFormInput("");
+      taskId.current++
+      setTasks([...tasks, newTask])
+      setFormInput('')
     }
-  };
+  }
 
   const handleComplete = (index) => {
-    const newTasks = [...tasks];
+    const newTasks = [...tasks]
     if (newTasks[index].completed === false) {
-      newTasks[index].completed = true;
+      newTasks[index].completed = true
     } else {
-      newTasks[index].completed = false;
+      newTasks[index].completed = false
     }
-    setTasks(newTasks);
-  };
+    setTasks(newTasks)
+  }
 
   const handleRemove = (index) => {
-    const newTasks = [...tasks];
-    newTasks.splice(index, 1);
-    setTasks(newTasks);
-  };
+    const newTasks = [...tasks]
+    newTasks.splice(index, 1)
+    setTasks(newTasks)
+  }
 
   const handleRemoveAll = () => {
-    setTasks([]);
-  };
+    setTasks([])
+  }
 
   return (
     <div className="app">
-      {/* <ThemeProvider theme={darkTheme}>
-        <Container maxWidth="sm"> */}
-      <header className="app-header">
-        <h2>Todo app powered by React + Vite</h2>
-      </header>
-      <TodoList
-        tasks={tasks}
-        handleComplete={handleComplete}
-        handleRemove={handleRemove}
-        handleRemoveAll={handleRemoveAll}
-      />
-      <TodoForm
-        formInput={formInput}
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-      />
-      {/* </Container>
-      </ThemeProvider> */}
-    </div>
-  );
-};
+      <ThemeProvider theme={lightTheme}>
+        <Container maxWidth="sm">
+          <header className="app-header">
+            <h2>Todo app powered by React + Vite</h2>
+          </header>
+          <Box
+            sx={{
+              backgroundColor: 'background.paper',
+            }}
+          >
+            <TodoList
+              tasks={tasks}
+              handleComplete={handleComplete}
+              handleRemove={handleRemove}
+              handleRemoveAll={handleRemoveAll}
+            />
 
-export default App;
+            <TodoForm
+              formInput={formInput}
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+            />
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </div>
+  )
+}
+
+export default App
--- react-todo-example/src/components/TodoList.jsx
+++ react-todo-example/src/components/TodoList.jsx
@@ -1,31 +1,53 @@
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Checkbox from '@mui/material/Checkbox'
+import IconButton from '@mui/material/IconButton'
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import SettingsIcon from '@mui/icons-material/Settings'
+import { ListSubheader, Switch } from '@mui/material'
+import { Box } from '@mui/system'
+
 const TodoList = (props) => {
-  const { tasks, handleComplete, handleRemove, handleRemoveAll } = props;
+  const { tasks, handleComplete, handleRemove, handleRemoveAll } = props
 
   return (
-    <ul className="todo">
+    <List
+      sx={{ width: '100%', bgcolor: 'background.paper' }}
+      subheader={
+        <ListSubheader>
+          <Box display={'flex'} justifyContent={'space-between'} padding={1} pr={0}>
+            <Box>My tasks</Box>
+            <Box>
+              Show completed
+              <Switch />
+            </Box>
+          </Box>
+        </ListSubheader>
+      }
+    >
       {tasks.map((task, index) => (
-        <li key={index}>
-          <div className="checkAndTask">
-            <label className="checkContainer">
-              <input type="checkbox" onClick={() => handleComplete(index)} />
-              <span className="checkmark"></span>
-            </label>
-            <span>{task.task}</span>
-          </div>
-          <button onClick={() => handleRemove(index)}>
-            Delete Todo
-          </button>
-        </li>
+        <ListItem
+          key={index}
+          secondaryAction={
+            <IconButton aria-label="delete">
+              <DeleteForeverIcon />
+            </IconButton>
+          }
+          disablePadding
+        >
+          <ListItemButton role={undefined} dense>
+            <ListItemIcon>
+              <Checkbox tabIndex={-1} disableRipple />
+            </ListItemIcon>
+            <ListItemText primary={task.task} />
+          </ListItemButton>
+        </ListItem>
       ))}
-      {tasks.length > 1 && (
-        <p>
-          <button className="delete-all" onClick={() => handleRemoveAll()}>
-            Delete all Todos
-          </button>
-        </p>
-      )}
-    </ul>
-  );
-};
+    </List>
+  )
+}
 
-export default TodoList;
+export default TodoList
