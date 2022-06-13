6c7a25900f3b9b9b449a1ca5055ef7e34d5a8dc5
Paul Csapak <paul@base34.com>
2022-06-13 12:07:51 +0200

    day-1: add nav

---
+++ react-component-example/src/Header.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+
+function Header() {
+  return (
+    <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        
+      </header>
+  );
+}
+
+export default Header;
---
+++ react-component-example/src/Nav.js
@@ -0,0 +1,41 @@
+function Nav() {
+    return (
+      <nav className="app-nav">
+        <ul>
+          <li>
+            <a
+              className="app-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              className="app-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              className="app-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+  
+  export default Nav
+  
