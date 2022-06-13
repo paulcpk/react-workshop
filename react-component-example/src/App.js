52013ddc549845eee3f9e17a4a472ad7b244c052
Paul Csapak <paul@base34.com>
2022-06-13 12:27:25 +0200

    day-1: add footer

--- react-component-example/src/App.js
+++ react-component-example/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ react-component-example/src/Footer.js
@@ -0,0 +1,7 @@
+function Footer() {
+  return (
+    <footer className="app-footer">Copyright Glory 2022</footer>
+  );
+}
+
+export default Footer;
