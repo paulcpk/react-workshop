434095511b574fff24dd4fedb1b35327642187eb
Paul Csapak <paul@base34.com>
2022-06-13 12:38:02 +0200

    day-1: add content

--- react-component-example/src/App.js
+++ react-component-example/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import Content from "./Content";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Content />
       <Footer />
     </div>
   );
---
+++ react-component-example/src/Content.js
@@ -0,0 +1,8 @@
+function Content() {
+    return (
+      <p>Here some content</p>
+    );
+  }
+  
+export default Content;
+  
