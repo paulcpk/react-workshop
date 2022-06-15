49e9a1f14a868541f4ff81d1d011b687f7fa0fd7
Paul Csapak <paul@base34.com>
2022-06-15 15:34:12 +0200

    day-3: update useForm example

--- next-hooks-example/pages/hooks/use-form.js
+++ next-hooks-example/pages/hooks/use-form.js
@@ -16,17 +16,30 @@ export default function App() {
       <main className={styles.main}>
         <h1>useForm()</h1>
         <form className={hookStyles.form} onSubmit={handleSubmit(onSubmit)}>
-          <input type="text" placeholder="First Name" {...register('firstName')} />
-          <input type="text" placeholder="Last Name" {...register('lastName')} />
-          <input type="number" placeholder="Age" {...register('age')} />
+          <input
+            type="text"
+            placeholder="First Name"
+            {...register("firstName")}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            {...register("lastName", { required: true })}
+          />
+          {errors.lastName && "Last name is required"}
+          
+          <input type="number" placeholder="Age" {...register("age", { min: 18, max: 99 })} />
+          {errors.age && "Age must be between 18 and 99"}
+
           <textarea
             placeholder="message"
             name=""
             id=""
             cols="30"
             rows="10"
-            {...register('message')}
+            {...register("message", { required: true })}
           ></textarea>
+          {errors.message && "Message name is required"}
 
           <button type="submit">Send message</button>
         </form>
