e3835fdee8b4863c58b4e175e2f7bdf20a93f1e9
Paul Csapak <paul@base34.com>
2022-10-31 11:51:38 +0100

    react-memoization-example: extract PostList component, introduce useCallback

--- react-memoization-example/src/App.jsx
+++ react-memoization-example/src/App.jsx
@@ -1,14 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import "./App.css";
 import * as db from "./db.json";
-
-const COLOR_ICONS = {
-  blue: <span role="img">🚙</span>,
-  gray: <span role="img">🚓</span>,
-  red: <span role="img">🚗</span>
-};
+import PostList, { COLOR_ICONS } from "./PostList";
 
 function App() {
+  const [count, setCount] = useState(0);
   const [posts, setPosts] = useState([]);
   const [expanded, setExpanded] = useState(0);
 
@@ -20,6 +16,24 @@ function App() {
     }, 500);
   }, []);
 
+  // we can assume this handlers do more complicated stuff
+  // and therefore should not be passed as inline functions
+  const resetHandler = () => {
+    setExpanded(0);
+  };
+
+  const setExpandedHandler = (id) => {
+    setExpanded(id);
+  };
+
+  // const resetHandler = useCallback(() => {
+  //   setExpanded(0);
+  // }, []);
+
+  // const setExpandedHandler = useCallback((id) => {
+  //   setExpanded(id);
+  // }, []);
+
   const postList = () => {
     console.log("run postList");
     if (!posts.length) {
@@ -27,27 +41,31 @@ function App() {
     }
 
     return (
-      <ul className="post-list">
-        {posts.map((post) => (
-          <li
-            key={post.id}
-            onClick={() => {
-              setExpanded(post.id);
-            }}
-          >
-            {COLOR_ICONS[post.specs.color]} {post.title}
-            {post.id === expanded && (
-              <ul className="post-spec-list">
-                {Object.keys(post.specs).map((key) => (
-                  <li key={post.specs[key]}>
-                    {key}: {post.specs[key]}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>
+      <>
+        <button onClick={resetHandler}>Reset expanded details</button>
+        <ul className="post-list">
+          {posts.map((post) => (
+            <li
+              key={post.id}
+              onClick={() => {
+                setExpandedHandler(post.id);
+              }}
+            >
+              {COLOR_ICONS[post.specs.color]} {post.title}
+              {post.id === expanded && (
+                <ul className="post-spec-list">
+                  {Object.keys(post.specs).map((key) => (
+                    <li key={post.specs[key]}>
+                      {key}: {post.specs[key]}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+        <p>Car count is {posts.length}</p>
+      </>
     );
   };
 
@@ -55,9 +73,18 @@ function App() {
     <div className="app">
       <h1>Memoization using useCallback</h1>
       <div className="card">
-        <button onClick={() => setExpanded(0)}>Reset expanded details</button>
-        {postList()}
-        <p>Car count is {posts.length}</p>
+        {/* {postList()} */}
+        <PostList
+          posts={posts}
+          expanded={expanded}
+          resetHandler={resetHandler}
+          setExpandedHandler={setExpandedHandler}
+        />
+      </div>
+      <div className="card">
+        <h2>Unrelated Counter method</h2>
+        <button onClick={() => setCount((curr) => curr + 1)}>Count +1</button>
+        <p>Current counter is {count}</p>
       </div>
     </div>
   );
